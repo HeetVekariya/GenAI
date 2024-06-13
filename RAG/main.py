024002de7c4842b2b185181f551a608b721bb979
HEET VEKAIRYA <heetvekariya50@gmail.com>
2024-06-13 23:04:17 +0530

    feat: simple RAG app

---
+++ RAG/main.py
@@ -0,0 +1,61 @@
+from langchain_community.document_loaders import HuggingFaceDatasetLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, pipeline
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.chains import RetrievalQA
+
+
+# Specify the dataset name and the column containing the content
+dataset_name = "databricks/databricks-dolly-15k"
+page_content_column = "context"  # or any other column you're interested in
+
+# Create a loader instance
+loader = HuggingFaceDatasetLoader(dataset_name, page_content_column)
+
+# Load the data
+data = loader.load()
+
+# # Display the first 15 entries
+# print(data[:2])
+
+
+
+# Create an instance of the RecursiveCharacterTextSplitter class with specific parameters.
+# It splits text into chunks of 1000 characters each with a 150-character overlap.
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+
+# print(text_splitter)
+
+# 'data' holds the text you want to split, split the text into documents using the text splitter.
+docs = text_splitter.split_documents(data)
+
+# print(docs[:2])
+
+
+
+# Define the path to the pre-trained model you want to use
+modelPath = "sentence-transformers/all-MiniLM-l6-v2"
+
+# Create a dictionary with model configuration options, specifying to use the CPU for computations
+model_kwargs = {'device':'cpu'}
+
+# Create a dictionary with encoding options, specifically setting 'normalize_embeddings' to False
+encode_kwargs = {'normalize_embeddings': False}
+
+# Initialize an instance of HuggingFaceEmbeddings with the specified parameters
+embeddings = HuggingFaceEmbeddings(
+    model_name=modelPath,     # Provide the pre-trained model's path
+    model_kwargs=model_kwargs, # Pass the model configuration options
+    encode_kwargs=encode_kwargs # Pass the encoding options
+)
+
+
+# text = "This is a test document."
+# query_result = embeddings.embed_query(text)
+# print(query_result[:3])
+
+
+db = FAISS.from_documents(docs, embeddings)
